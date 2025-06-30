261368dcc021a9049f1b322de16612cb837f564e
Anontux <the_newrich@yahoo.com>
2025-06-30 21:41:50 +0200

    Update db.php

--- core/db.php
+++ core/db.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER', '127.0.0.1');
-define('DB_USERNAME', 'nounblx');
-define('DB_PASSWORD', '3myhnqpe');
-define('DB_NAME', 'nounblx');
+define('DB_USERNAME', 'ArbexBlox');
+define('DB_PASSWORD', 'AmongUsRevival');
+define('DB_NAME', 'ArbexBlox');
 
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
